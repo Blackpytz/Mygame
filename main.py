209e1ce2ed8ec6a816a678bcd14360d3c7db39f2
Reid-C <theophiluschikezie@gmail.com>
2023-12-26 12:38:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (200,100))
+
+    pygame.display.update()
+    clock.tick(60)
