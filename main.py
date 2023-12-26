d070db165347215fde888c18512685a911e38c08
Reid-C <theophiluschikezie@gmail.com>
2023-12-26 21:55:30 +0100

    Update my text and add collision

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font('Runner_fonts/pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('Runner_graphics/Sky.png').convert()
 ground_surface = pygame.image.load('Runner_graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surf = test_font.render('My game', False, 'Black')
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load('Runner_graphics/Snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600,300))
@@ -22,17 +24,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                print('Collision')
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (320,50))
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
-
     screen.blit(snail_surf, snail_rect)
     screen.blit(player_surf, player_rect)
+
+    #if player_rect.colliderect(snail_rect):
+        
+
 
     pygame.display.update()
     clock.tick(60)
