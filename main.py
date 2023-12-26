7fa40c172ffa615e5eee3ca18246f2156100af26
Reid-C <theophiluschikezie@gmail.com>
2023-12-26 13:22:16 +0100

    Added ground surface

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('Runner_graphics/Sky.png')
+ground_surface = pygame.image.load('Runner_graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +15,8 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (200,100))
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
 
     pygame.display.update()
     clock.tick(60)
