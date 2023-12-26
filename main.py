57342e2a3ccabf56e8840e6541c47904364ba861
Reid-C <theophiluschikezie@gmail.com>
2023-12-26 13:39:39 +0100

    Added font to game

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('Runner_fonts/pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('Runner_graphics/Sky.png')
 ground_surface = pygame.image.load('Runner_graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Blue')
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +19,7 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (320, 50))
 
     pygame.display.update()
     clock.tick(60)
