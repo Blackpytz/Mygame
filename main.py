6d3976923202c2224ee161c12e74ea1d10fb93e5
Reid-C <theophiluschikezie@gmail.com>
2023-12-30 06:40:55 +0100

    Add music to game

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom = (80, 300))
         self.gravity = 0
 
+        self.jump_sound = pygame.mixer.Sound('Runner_audio/jump.mp3')
+        self.jump_sound.set_volume(0.5)
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
@@ -111,7 +115,10 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('Runner_fonts/pixeltype.ttf', 50)
 game_active = False
 start_time = 0
-score = 0 
+score = 0
+bg_music = pygame.mixer.Sound('Runner_audio/music.wav')
+bg_music.play(loops = -1)
+bg_music.set_volume(0.3)
 
 #Groups
 player = pygame.sprite.GroupSingle()
